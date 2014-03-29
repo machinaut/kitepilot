0bf05c1e3e57633cc0890f6630e38cf52c5fa930
Alex Ray <a@machinaut.com>
2014-03-29 03:05:18 -0700

    opencv: basic demo code
    capture.py shows basic capture & save functionality
    video.py shows basic video processing
    
    TODO: color shift and threshold the image.
    Ref:
    http://opencv-srf.blogspot.ro/2010/09/object-detection-using-color-seperation.html

---
+++ opencv/capture.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# capture.py - demo basic capture functionality
+
+# need opencv installed. see http://opencv.org for more
+import cv2
+
+# Laptop webcam is '0', and our usb camera is probably '1'
+# IF THIS DOESNT WORK TRY ANOTHER NUMBER :)
+webcam = cv2.VideoCapture(1)
+
+rval, frame = webcam.read()
+if not rval:
+    print "ERROR CAPTURING FRAME"
+    exit(1)
+
+# Save the frame
+cv2.imwrite('frame.jpg', frame)
+
+# Display the frame
+cv2.namedWindow('capture demo')
+cv2.imshow('capture demo', frame)
+print "Captured dimensions: ",frame.shape
+print "Press a key (in the window) to exit."
+cv2.waitKey(0) # wait for a keypress
+cv2.destroyAllWindows() # nuke it all before exit
---
+++ opencv/video.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# video.py - demo basic video processing
+
+# need opencv installed. see http://opencv.org for more
+import cv2, datetime
+
+# Laptop webcam is '0', and our usb camera is probably '1'
+webcam = cv2.VideoCapture(1)
+
+# get some basic info about the camera
+width  = webcam.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+height = webcam.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+print "Webcam width: ", width, " height: ", height
+
+start = datetime.datetime.now()
+
+# loop a bunch then exit
+for i in range(100):
+    rval, frame = webcam.read()
+    if not rval:
+        print "Error reading image!"
+        exit(1)
+
+    # smooth original image using 3x3 gaussian kernel
+    cv2.GaussianBlur(frame, (3,3), 0)
+
+end = datetime.datetime.now()
+diff = end - start
+# I'm getting about 9.9 seconds for 100 frames on my macbook
+print "100 runs in", diff.seconds + diff.microseconds*1E-6, "seconds"
