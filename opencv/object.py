91938b0e7ad701d2b5af785fe369de346cde435b
Michael Bella <mike1000@Michaels-Laptop.local>
2014-03-29 17:40:03 -0700

    I hacked a bunch of junk in here!!!!!
    The script now tracks green things by drawing a circle around them. For
    our purposes the center of this circle is the COM of the object we are
    tracking.

--- opencv/object.py
+++ opencv/object.py
@@ -2,18 +2,26 @@
 import cv2
 import numpy as np
 import datetime
+import glob
+
+VID_FILE = True
 
 # Run at VGA resolution
-webcam = cv2.VideoCapture(1)
+webcam = cv2.VideoCapture(0)
 webcam.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,640)
 webcam.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,480)
 
 # output window
 cv2.namedWindow('foo')
 
-# testing with a mandarin orange
-color_min = np.array([7,50,50], np.uint8)
-color_max = np.array([12,255,255], np.uint8)
+# testing with a green iPhone 5c
+color_min = np.array([50,60,60], np.uint8)
+color_max = np.array([70,255,255], np.uint8)
+
+# Open a Video File??????
+if VID_FILE:
+    print glob.glob('~/Downloads/*')
+    cap = cv2.VideoCapture('/Users/mike1000/Downloads/KITE.mov')
 
 # Measure time for science!
 past = datetime.datetime.now()
@@ -27,13 +35,17 @@ while True:
     past = current
 
     # capture image from camera
-    rv,img = webcam.read()
+    if VID_FILE:
+        rv, img = cap.read()
+    else:
+        rv, img = webcam.read()
+
     if not rv:
         print "ERROR capturing image"
         exit(1)
 
     # TODO: this may be overkill on the blur
-    cv2.GaussianBlur(img,(21,21),0)
+    cv2.GaussianBlur(img,(15,15),0)
 
     # Convert to HSV before threshold
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -41,5 +53,47 @@ while True:
     # Threshold within csv
     thresh = cv2.inRange(hsv, color_min, color_max)
 
+    thresh1 = thresh.copy()
+
+    cv2.GaussianBlur(thresh1,(3,3),0)
+
+
+    # Get the contours...
+    #cont = cv2.Canny(thresh, 10, 200)
+    #contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_TC89_KCOS)
+    
+    #print contours
+    try:
+        cnt = contours[0]
+        #cv2.drawContours(thresh, [cnt], 0, (0,255,0), -1)
+        #print cnt
+        
+        M = cv2.moments(cnt)
+        #print M
+        
+        max_area = 0
+        # Find the largest BLOB
+        for ind, blb in enumerate(contours):
+            area = cv2.contourArea(blb)
+            if area > max_area  :
+                max_area = area
+                max_index = ind
+        
+        #print area
+        big_blob = contours[max_index]
+        
+        (x,y),radius = cv2.minEnclosingCircle(big_blob)
+        center = (int(x),int(y))
+        radius = int(radius)
+        cv2.circle(img,center,radius,(0,255,0),2)      
+        print center
+    except Exception, e:
+        print 'NOPE', e
+
+
     # Show the result
-    cv2.imshow('foo', thresh)
+    
+    cv2.imshow('foo', thresh1)
+    cv2.imshow('bar', img)
+  
