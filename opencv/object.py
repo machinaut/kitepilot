6dfee2b5bc353130f40ae9e851f771e25c3b882d
PenguiNx42 <taylork@alum.mit.edu>
2014-03-30 13:35:29 -0700

    added serial control
    have fun

--- opencv/object.py
+++ opencv/object.py
@@ -6,6 +6,94 @@ import glob
 import time
 from collections import deque
 
+import math
+import serial
+
+def open_arduino():
+    ser = serial.Serial('COM7', 9600)
+
+    #wait for arduino to reset
+    time.sleep(3)
+
+    #send a newline to tell arduino to start
+    ser.write('\n')
+        
+    #wait for GO (calibration done)
+    while "GO" not in ser.readline():
+        pass
+        
+    return ser
+    
+# normalize degrees
+def norm(x):
+    x = x % 360.
+    return x if x < 180. else x - 360.
+    
+# get angle towards the center
+def tocent(centerx,centery,x,y):
+    return math.degrees(math.atan2(centery-y,centerx-x))
+    
+center_thresh = 50
+
+# close enough to center
+def closecent(centerx,centery,x,y):
+    return abs(y - centery) < center_thresh and abs(x - centerx) < center_thresh:
+    
+widthfactor = 0.1
+
+## states:
+## 0 upper right, aim down (cw)
+## 1 lower right, aim to center
+## 2 away from center up left 45 deg
+## 3 upper left, aim down (ccw)
+## 4 lower left, aim to center
+## 5 away from center up right 45 deg
+## please give normalized angle as input
+## outputs angle error
+def next_state(state, x, y, centerx, centery, width, angle):
+    next = state
+    angle_err = 0.
+    
+    if state == 0: # turn right
+        angle_err = -90. - angle #don't normalize, we want the quadrant kink
+        if y < centery and angle < 0:
+            next = 1
+            
+    elif state == 1: #back to center
+        angle_err = norm(tocent(centerx, centery, x, y) - angle)
+        if closecent(centerx, centery, x, y):
+            next = 2
+            
+    elif state == 2: #away from center left
+        angle_err = norm(135 - angle)
+        if x < (centerx - width * widthfactor) and y > centery:
+            next = 3
+            
+    elif state == 3: #turn left
+        if angle > 0:
+            temp_angle = angle - 360 #move the quadrant kink over
+        else:
+            temp_angle = angle
+        angle_err = -90. - temp_angle #don't normalize, we want the quadrant kink
+        if y < centery and angle < 0:
+            next = 4
+            
+    elif state == 4: #back to center
+        angle_err = norm(tocent(centerx, centery, x, y) - angle)
+        if closecent(centerx, centery, x, y):
+            next = 5
+            
+    elif state == 5: #away from center right
+        angle_err = norm(45 - angle)
+        if x > (centerx - width * widthfactor) and y > centery:
+            next = 0
+            
+    else:
+        next_state = 0
+        goal_angle = -90
+        
+    return (next, angle_err)
+
 VID_FILE = False
 
 VGA_W = 640
@@ -33,6 +121,12 @@ past = datetime.datetime.now()
 
 # Path History
 blob_hist = deque([(0, 0)])
+
+state = 0
+ser = open_arduino()
+
+ser_delay = 3 # update serial ~10fps (assuming loop runs at 30fps)
+ser_count = 0
 
 # loop until forever
 while True:
@@ -132,4 +226,24 @@ while True:
     
     cv2.imshow('foo', thresh1)
     cv2.imshow('bar', img)
+    
+    #at this point I want 
+    # x, y, centerx, centery, width, angle
+    # x is right
+    # y is up
+    # angles are degrees 0 pointing along x, increase CCW
+    
+    angle = norm(angle)
+    
+    # control state machine
+    state, err = next_state(state, x, y, centerx, centery, width, angle)
+    
+    # control motor
+    if ser_count > ser_delay:
+        pos = str(int((err/180.)*512.+512.))
+        ser.write(pos + '\n')
+        print pos, err
+        ser_count = 0
+    else:
+        ser_count = ser_count + 1
   
