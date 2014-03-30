3648c81a5f2bbe388bc4c4bd2be50dec4d6f56ba
Alex Ray <a@machinaut.com>
2014-03-29 23:39:43 -0700

    need to rewrite as state machine

--- pygame/fly.py
+++ pygame/fly.py
@@ -6,10 +6,15 @@ pygame.init()
 # make sure we run at most at a specific FPS
 fps = pygame.time.Clock()
 
+# normalize degrees
+def norm(x):
+    x = x % 360.
+    return x if x < 180. else x - 360.
+
 surface = pygame.display.set_mode((1280,720))
 # Target path center and width
 # TODO: this gets processed by a "pre-flight"
-center = (1280/2, 250)
+center = (1280/2, 520)
 width  = 500.0
 
 sky = pygame.Color(50,242,255)
@@ -23,7 +28,13 @@ angle = 45 # degrees CCW of "right" along the horizon
 x = center[0]
 y = center[1]
 
-t = 0.0
+# get angle towards the center
+def tocent():
+    return math.degrees(math.atan2(center[1]-y,center[0]-x))
+
+# approx turn rate
+turn = 14.0
+
 # loop until forever. goddamn robots.
 while True:
     fps.tick(60) # go at most 60FPS
@@ -31,18 +42,28 @@ while True:
     x += int(math.sin(math.radians(angle))*wind)
     y += int(math.cos(math.radians(angle))*wind)
     # calculate new angle
-    if x < (center[0] - width * .14): # turn left
-        angle -= 20.0
-    elif x < (center[0] + width * .14): # aim for 45
-        if abs(angle - 45) < 90: # angle was closer to 45 than -45
-            angle = 45.0
-        else:
-            angle = -45.0
+    if x < (center[0] - width * .14): # turn left (left side)
+        if y > center[1]: # upper half
+            angle = max(norm(angle - turn), tocent())
+        else: # lower half
+            if angle > 0: # upwards
+                angle = max(norm(angle - turn), 90)
+            else: # downwards
+                angle = norm(angle - turn)
+    elif x > (center[0] + width * .14): # turn right (right side)
+        if y > center[1]: # upper half
+            angle = max(norm(angle - turn), -90.0)
+        else: # lower half
+            if angle > 0: # upwards
+                angle = min(norm(angle - turn),tocent())
+            else: # downwards
+                angle = norm(angle - turn)
     else: # x > (center[0] + width * .14)
-        angle += 20.0
+        pass
 
     # draw the sky
     surface.fill(sky)
     # draw the kite
-    pygame.draw.circle(surface, grn, (x,y), 44, 0)
+    # DRAW FROM A BETTER COORDINATE SYSTEM
+    pygame.draw.circle(surface, grn, (x, 720-y), 44, 0)
     pygame.display.update()
