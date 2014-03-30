2b14c5c7f3e58bdefa2a1954797f752e8fc62433
PenguiNx42 <taylork@alum.mit.edu>
2014-03-29 19:19:49 -0700

    Decent control
    Follows potentiometer. Jerky if moved slowly. No endstops yet.

--- MotorDemo/MotorDemo.ino
+++ MotorDemo/MotorDemo.ino
@@ -25,10 +25,10 @@ long goalp = 0;
 long usec_per_step = 0; // 0 is special and means stop
 long goal_ups = 0; // current goal
 const long max_ups = 1000; // min speed before stopping
-const long min_ups = 50; // max speed
-const long usec_per_ups = 100; // "acceleration?" (usec / (usec/step))
-const long usec_per_ctrl = 50000; // control rate for serial i/o etc
-const long close_pos = 300;
+const long min_ups = 500; // max speed
+const long usec_per_ups = 500; // "acceleration?" (usec / (usec/step))
+const long usec_per_ctrl = 100000; // control rate for serial i/o etc
+const long close_pos = 200;
 
 void calibrate() {
   int stop1;
@@ -43,7 +43,8 @@ void calibrate() {
   stop2 = !digitalRead(stop2Pin);
   while(!stop1 && !stop2) {
     delay(1);
-    digitalWrite(led, ledState = (ledState==LOW)?HIGH:LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(led, LOW);
     pos++;
     stop1 = !digitalRead(stop1Pin);
     stop2 = !digitalRead(stop2Pin);
@@ -66,7 +67,8 @@ void calibrate() {
 
   while((!stop1 || got1) && (!stop2 || got2)) {
     delay(1);
-    digitalWrite(led, ledState = (ledState==LOW)?HIGH:LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(led, LOW);
     pos--;
     //Serial.println(pos);
     stop1 = !digitalRead(stop1Pin);
@@ -93,6 +95,7 @@ void setup() {
   pinMode(PwrPin, OUTPUT);
   digitalWrite(PwrPin, HIGH);
   digitalWrite(dirPin, HIGH);
+  digitalWrite(led, LOW);
   pinMode(enPin, OUTPUT);
   enState = LOW;
   digitalWrite(enPin, enState);
@@ -102,7 +105,7 @@ void setup() {
 
 void loop() {
   unsigned long current = micros();
-  
+
   unsigned long interval_ctrl = (current - previous_ctrl);
   // run control i/o
   if(interval_ctrl > usec_per_ctrl) {
@@ -111,16 +114,17 @@ void loop() {
     //double goalv = ((potpos - 512)/512.0)*maxv;
     goalp = (potpos/1024.0)*(stop2Pos-stop1Pos) + stop1Pos;
 
-    // go toward goal until close enough
-    // when close, slow down
-    long dist = pos-goalp;
-    if(abs(dist)>close_pos) {
-      if(dist < 0) goal_ups = min_ups;
-      else if(dist > 0) goal_ups = -min_ups;
-    }
-    else goal_ups = 0;
+    //Serial.print(goalp);
+    //Serial.print("     ");
+    Serial.print(pos);
+    Serial.print("     ");
+    //Serial.print(goal_ups);
+    //Serial.print("     ");*/
+    Serial.println(usec_per_step);
+    //Serial.print("     ");
+    //Serial.println("");
   }
-  
+
   //Serial.println(usec_per_step);
 
   unsigned long interval_step = (current - previous_step);
@@ -128,12 +132,13 @@ void loop() {
   if(usec_per_step != 0) { // 0 means stop!
     if (interval_step > abs(usec_per_step)) {
       previous_step = current;
-      usec_per_step = (usec_per_step>0?1:-1)*interval_step;
+      //usec_per_step = (usec_per_step>0?1:-1)*interval_step;
       /*if(interval_step > max_ups)
-        usec_per_step = (usec_per_step>0?1:-1)*max_ups;
-      else
-        usec_per_step = (usec_per_step>0?1:-1)*interval_step;*/
-      digitalWrite(led, ledState = (ledState==LOW)?HIGH:LOW);
+       usec_per_step = (usec_per_step>0?1:-1)*max_ups;
+       else
+       usec_per_step = (usec_per_step>0?1:-1)*interval_step;*/
+      digitalWrite(led, HIGH);
+      digitalWrite(led, LOW);
       if(dirState == HIGH) pos++;
       else pos--;
     }
@@ -145,6 +150,17 @@ void loop() {
   // update velocity according to max acceleration
   if (interval_accel > usec_per_ups){
     previous_accel = current;
+    
+    // set goal velocity
+    // head toward goal position until close enough
+    long dist = pos-goalp;
+    if(abs(dist)>close_pos) {
+      if(dist < 0) goal_ups = min_ups;
+      else if(dist > 0) goal_ups = -min_ups;
+    }
+    else goal_ups = 0;
+    
+    // control velocity ramp based on goal velocity
     if (goal_ups == 0) {
       // going toward stop, go to maximum ups (will be set to stop below)
       if(usec_per_step>0) usec_per_step++;
@@ -172,27 +188,19 @@ void loop() {
 
     if(abs(usec_per_step) > max_ups) usec_per_step = 0; // stop!
     if(abs(usec_per_step) < min_ups && usec_per_step != 0) usec_per_step = min_ups; // ceiling
-    
+
     if(usec_per_step < 0) dirState = LOW;
     else dirState = HIGH;
-  
+
     digitalWrite(dirPin, dirState);
   }
 
   ////Serial.println("STEP");
 
   ////Serial.println(potpos);
-  /*
-  Serial.print(goalp);
-  Serial.print("     ");
-  Serial.print(pos);
-  Serial.print("     ");
-  Serial.print(goal_ups);
-  Serial.print("     ");
-  Serial.print(usec_per_step);
-  Serial.print("     ");
-  Serial.println("");*/
+
 }
+
 
 
 
